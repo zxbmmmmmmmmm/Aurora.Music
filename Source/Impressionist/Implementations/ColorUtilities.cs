d6724c1bcdb55a7c1c2fd69975c4d5304e16dd8c
Betta_Fish <zxbmmmmmmmmm@qq.com>
2024-07-08 17:39:30 +0800

    feat: Add Isolation background

---
+++ Source/Impressionist/Implementations/ColorUtilities.cs
@@ -0,0 +1,107 @@
+﻿using Colourful;
+using Impressionist.Abstractions;
+using System;
+using System.Numerics;
+
+namespace Impressionist.Implementations
+{
+    public static class ColorUtilities
+    {
+        public static HSVColor RGBVectorToHSVColor(this Vector3 color)
+        {
+            HSVColor hsv = new HSVColor();
+
+            float max = Math.Max(Math.Max(color.X, color.Y), color.Z);
+            float min = Math.Min(Math.Min(color.X, color.Y), color.Z);
+
+            hsv.V = max * 100 / 255;
+
+            if (max == min)
+            {
+                hsv.H = 0;
+                hsv.S = 0;
+            }
+            else
+            {
+                hsv.S = (((max - min) / max) * 100);
+
+                hsv.H = 0;
+
+                if (max == color.X)
+                {
+                    hsv.H = (60 * (color.Y - color.Z) / (max - min));
+                    if (hsv.H < 0) hsv.H += 360;
+                }
+                else if (max == color.Y)
+                {
+                    hsv.H = (60 * (2 + (color.Z - color.X) / (max - min)));
+                    if (hsv.H < 0) hsv.H += 360;
+                }
+                else if (max == color.Z)
+                {
+                    hsv.H = (60 * (4 + (color.X - color.Y) / (max - min)));
+                    if (hsv.H < 0) hsv.H += 360;
+                }
+
+            }
+            return hsv;
+        }
+        public static Vector3 HSVColorToRGBVector(this HSVColor hsv)
+        {
+            if (hsv.H == 360) hsv.H = 0;
+            int Hi = (int)Math.Floor((float)hsv.H / 60) % 6;
+
+            float f = (hsv.H / 60) - Hi;
+            float p = (hsv.V / 100) * (1 - (hsv.S / 100));
+            float q = (hsv.V / 100) * (1 - f * (hsv.S / 100));
+            float t = (hsv.V / 100) * (1 - (1 - f) * (hsv.S / 100));
+
+            p *= 255;
+            q *= 255;
+            t *= 255;
+
+            Vector3 rgb = Vector3.Zero;
+
+            switch (Hi)
+            {
+                case 0:
+                    rgb = new Vector3(hsv.V * 255 / 100, t, p);
+                    break;
+                case 1:
+                    rgb = new Vector3(q, hsv.V * 255 / 100, p);
+                    break;
+                case 2:
+                    rgb = new Vector3(p, hsv.V * 255 / 100, t);
+                    break;
+                case 3:
+                    rgb = new Vector3(p, q, hsv.V * 255 / 100);
+                    break;
+                case 4:
+                    rgb = new Vector3(t, p, hsv.V * 255 / 100);
+                    break;
+                case 5:
+                    rgb = new Vector3(hsv.V * 255 / 100, p, q);
+                    break;
+            }
+
+            return rgb;
+        }
+        internal static Vector3 RGBColorToRGBVector(this RGBColor color)
+        {
+            return new Vector3((float)color.R, (float)color.G, (float)color.B);
+        }
+        internal static RGBColor RGBVectorToRGBColor(this Vector3 color)
+        {
+            return new RGBColor(color.X, color.Y, color.Z);
+        }
+        internal static Vector3 LABColorToLABVector(this LabColor color)
+        {
+            return new Vector3((float)color.L, (float)color.a, (float)color.b);
+        }
+        internal static LabColor LABVectorToLABColor(this Vector3 color)
+        {
+            return new LabColor(color.X, color.Y, color.Z);
+        }
+
+    }
+}
---
+++ Source/Impressionist/Implementations/KMeansPaletteGenerator.cs
@@ -0,0 +1,211 @@
+﻿using Colourful;
+using Impressionist.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Impressionist.Implementations
+{
+    // I'm really appreciate wieslawsoltes's PaletteGenerator. Which make this project possible.
+    public class KMeansPaletteGenerator :
+        IThemeColorGenrator,
+        IPaletteGenrator
+    {
+        private IColorConverter<RGBColor, LabColor> _labColorConverter = new ConverterBuilder().FromRGB().ToLab().Build();
+        private IColorConverter<LabColor, RGBColor> _rgbColorConverter = new ConverterBuilder().FromLab().ToRGB().Build();
+        public Task<ThemeColorResult> CreateThemeColor(Dictionary<Vector3, int> sourceColor, bool ignoreWhite = false, bool toLab = false)
+        {
+            var builder = sourceColor.AsEnumerable();
+            if (ignoreWhite && sourceColor.Count > 1)
+            {
+                builder = builder.Where(t => t.Key.X <= 250 || t.Key.Y <= 250 || t.Key.Z <= 250);
+            }
+            if (toLab)
+            {
+                builder = builder.Select(t=>new KeyValuePair<Vector3, int>(_labColorConverter.Convert(t.Key.RGBVectorToRGBColor()).LABColorToLABVector(),t.Value));
+            }
+            var targetColor = builder.ToDictionary(t => t.Key, t => t.Value);
+            var clusters = KMeansCluster(targetColor, 1, false);
+            var colorVector = clusters.First().OrderByDescending(t => t.Value).First().Key;
+            if (toLab)
+            {
+                colorVector = _rgbColorConverter.Convert(clusters.First().OrderByDescending(t => t.Value).First().Key.LABVectorToLABColor()).RGBColorToRGBVector();
+            }
+            var isDark = colorVector.RGBVectorToHSVColor().V <= 50f;
+            return Task.FromResult(new ThemeColorResult(colorVector, isDark));
+        }
+
+        public async Task<PaletteResult> CreatePalette(Dictionary<Vector3, int> sourceColor, int clusterCount, bool ignoreWhite = false, bool toLab = false, bool useKMeansPP = false)
+        {
+            if (sourceColor.Count == 1)
+            {
+                ignoreWhite = false;
+            }
+            var colorResult = await CreateThemeColor(sourceColor, ignoreWhite, toLab);
+            var builder = sourceColor.AsEnumerable();
+            var colorIsDark = colorResult.ColorIsDark;
+            if (colorIsDark)
+            {
+                builder = builder.Where(t => t.Key.RGBVectorToHSVColor().V < 50);
+            }
+            else
+            {
+                if (!ignoreWhite)
+                {
+                    builder = builder.Where(t => t.Key.RGBVectorToHSVColor().V >= 50);
+                }
+                else
+                {
+                    builder = builder.Where(t => t.Key.RGBVectorToHSVColor().V >= 50 && (t.Key.X <= 250 || t.Key.Y <= 250 || t.Key.Z <= 250));
+                }
+            }
+            if (toLab)
+            {
+                builder = builder.Select(t => new KeyValuePair<Vector3, int>(_labColorConverter.Convert(t.Key.RGBVectorToRGBColor()).LABColorToLABVector(), t.Value));
+            }
+            var targetColors = builder.ToDictionary(t => t.Key, t => t.Value);
+            var clusters = KMeansCluster(targetColors, clusterCount, useKMeansPP);
+            var dominantColors = new List<Vector3>();
+            foreach (var cluster in clusters)
+            {
+                var representative = cluster.OrderByDescending(c => c.Value).First().Key;
+                if (toLab)
+                {
+                    representative = _rgbColorConverter.Convert(representative.LABVectorToLABColor()).RGBColorToRGBVector();
+                }
+                dominantColors.Add(representative);
+            }
+            var result = new List<Vector3>();
+            var count = dominantColors.Count;
+            for (int i = 0; i < clusterCount; i++)
+            {
+                // You know, it is always hard to fullfill a palette when you have no enough colors. So please forgive me when placing the same color over and over again.
+                result.Add(dominantColors[i % count]);
+            }
+            return new PaletteResult(result, colorIsDark, colorResult);
+        }
+        static List<Dictionary<Vector3, int>> KMeansCluster(Dictionary<Vector3, int> colors, int numClusters, bool useKMeansPP)
+        {
+            // Initialize the clusters, reduces the total number when total colors is less than clusters
+            var clusterCount = Math.Min(numClusters, colors.Count);
+            var clusters = new List<Dictionary<Vector3, int>>();
+            for (int i = 0; i < clusterCount; i++)
+            {
+                clusters.Add(new Dictionary<Vector3, int>());
+            }
+
+            // Select the initial cluster centers randomly
+            Vector3[] centers = null;
+            if (!useKMeansPP)
+            {
+                centers = colors.Keys.OrderByDescending(t => Guid.NewGuid()).Take(clusterCount).ToArray();
+            }
+            else
+            {
+                centers = KMeansPlusPlusCluster(colors, clusterCount).ToArray();
+            }
+            // Loop until the clusters stabilize
+            var changed = true;
+            while (changed)
+            {
+                changed = false;
+                // Assign each color to the nearest cluster center
+                foreach (var color in colors.Keys)
+                {
+                    var nearest = FindNearestCenter(color, centers);
+                    var clusterIndex = Array.IndexOf(centers, nearest);
+                    clusters[clusterIndex][color] = colors[color];
+                }
+
+                // Recompute the cluster centers
+                for (int i = 0; i < Math.Min(numClusters, clusterCount); i++)
+                {
+                    var sumX = 0f;
+                    var sumY = 0f;
+                    var sumZ = 0f;
+                    var count = 0f;
+                    foreach (var color in clusters[i].Keys)
+                    {
+                        sumX += color.X;
+                        sumY += color.Y;
+                        sumZ += color.Z;
+                        count++;
+                    }
+
+                    var x = (sumX / count);
+                    var y = (sumY / count);
+                    var z = (sumZ / count);
+                    var newCenter = new Vector3(x, y, z);
+                    if (!newCenter.Equals(centers[i]))
+                    {
+                        centers[i] = newCenter;
+                        changed = true;
+                    }
+                }
+            }
+
+            // Return the clusters
+            return clusters;
+        }
+
+        static Vector3 FindNearestCenter(Vector3 color, Vector3[] centers)
+        {
+            var nearest = centers[0];
+            var minDist = float.MaxValue;
+
+            foreach (var center in centers)
+            {
+                var dist = Vector3.Distance(color, center); // The original version implemented a Distance method by wieslawsoltes himself, I changed that to Vector ones.
+                if (dist < minDist)
+                {
+                    nearest = center;
+                    minDist = dist;
+                }
+            }
+
+            return nearest;
+        }
+
+        static List<Vector3> KMeansPlusPlusCluster(Dictionary<Vector3, int> colors, int numClusters)
+        {
+            Random random = new Random();
+            var clusterCount = Math.Min(numClusters, colors.Count);
+            var clusters = new List<Vector3>();
+            var targetColor = colors.Keys.ToList();
+            var index = random.Next(targetColor.Count);
+            clusters.Add(targetColor[index]);
+            for (int i = 1; i < clusterCount; i++)
+            {
+                float accumulatedDistances = 0f;
+                float[] accDistances = new float[targetColor.Count];
+                for (int vectorId = 0; vectorId < targetColor.Count; vectorId++)
+                {
+                    var minDistanceItem = clusters[0];
+                    var minDistance = Vector3.Distance(minDistanceItem, targetColor[vectorId]);
+                    for (int clusterIdx = 1; clusterIdx < i; clusterIdx++)
+                    {
+                        float currentDistance = Vector3.Distance(clusters[clusterIdx], targetColor[vectorId]);
+                        if (currentDistance < minDistance)
+                        {
+                            minDistance = currentDistance;
+                        }
+                        accumulatedDistances += minDistance * minDistance;
+                        accDistances[vectorId] = accumulatedDistances;
+                    }
+                }
+                float targetPoint = (float)random.NextDouble() * accumulatedDistances;
+                for (int vectorId = 0; vectorId < targetColor.Count; vectorId++)
+                {
+                    if (accDistances[vectorId] >= targetPoint)
+                    {
+                        clusters.Add(targetColor[vectorId]);
+                        break;
+                    }
+                }
+            }
+            return clusters;
+        }
+    }
+}
